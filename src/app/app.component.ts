3f9a321264b9ede0b023914b2733449fc451d5bf
Matias Tomas Svec <tomassvec0@gmail.com>
2025-02-25 15:49:13 -0300

    W

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { ProjectCardComponent } from "./components/project-card/project-card.component";
 import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ProjectCardComponent, FooterComponent],
+  imports: [HeaderComponent, ProjectCardComponent, FooterComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
